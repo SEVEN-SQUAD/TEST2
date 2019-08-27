974684ce196dc99af6fa29a3b395ad7c7acf876b
SEVEN-SQUAD <54578023+SEVEN-SQUAD@users.noreply.github.com>
2019-08-27 18:30:15 +0100

    Add files via upload

---
+++ spam.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const client2 = new Discord.Client();
+const myid = ['609036773468667974'];
+const prefix = ['.'];
+
+client.on('ready', () => {
+   console.log(----------------);
+   console.log(Credit Farmm - Script By : 2-2=5);
+   console.log(----------------);
+   console.log(Loadinng);
+   console.log(Loadinng.);
+   console.log(Loadinng..);
+   console.log(Loadinng...);
+   console.log('This Bots Online ');
+   console.log(----------------);
+client.on('message', message => {
+if (message.content === prefix+'spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 9000000; x++) {
+        message.channel.send(**spam by seven   **[ " ${x} " ])
+          .then(m => {
+            count++;
+          })
+
+        }
+      }
+});
+
+client.login("");
